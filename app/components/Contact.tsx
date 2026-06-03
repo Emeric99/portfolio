64d8c62e6bd7e9fbfcd479231bdf38da23483e59
Emeric Tcholagheu <emerictcholagheu@gmail.com>
2026-06-03 23:34:45 +0200

    Add real CV PDF

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -55,7 +55,7 @@ const links = [
   {
     label: "CV",
     value: "Download CV",
-    href: "/cv.pdf",
+    href: "/Lebenslauf_Tcholagheu.pdf",
     icon: (
       <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3M3 17V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
